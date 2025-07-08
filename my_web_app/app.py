689b24eea78193f525ccf1f60c6904f4856f592a
David KUO <david.kuo@suijun.com>
2025-07-08 17:32:40 +0800

    new file

--- my_web_app/app.py
+++ my_web_app/app.py
@@ -1,17 +1,16 @@
 # app.py
 
-from flask import Flask
+from flask import Flask, render_template # 導入 render_template
+from datetime import datetime # 導入 datetime 模組來獲取當前日期
 
-# 建立一個 Flask 應用程式實例
 app = Flask(__name__)
 
-# 定義一個路由 (Route)
-# 當使用者訪問網站的根目錄 (例如: http://127.0.0.1:5000/) 時，會執行這個函式
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    # 獲取當前日期
+    today = datetime.now().strftime("%Y年%m月%d日 %H:%M:%S")
+    # 渲染 index.html 模板，並將 today 變數傳遞給模板
+    return render_template('index.html', current_date=today)
 
-# 啟動 Flask 應用程式
-# 如果這個檔案是直接被執行的 (而不是被其他檔案導入的)，就執行 app.run()
 if __name__ == '__main__':
-    app.run(debug=True) # debug=True 會在程式碼修改時自動重載，方便開發
+    app.run(debug=True)
